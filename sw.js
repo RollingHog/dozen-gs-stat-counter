afd9cfdaf5fe9ec7b173a4e54644f9ff7f1fd9b0
RollingHog <46373241+RollingHog@users.noreply.github.com>
2022-05-01 06:18:20 +0300

    sw possible offline fix

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const APP_PREFIX = 'rh_ghp_dsc'
 // The version of the cache. Every time you change any of the files
 // you need to change this version (version_01, version_02…).
 // eslint-disable-next-line no-unused-vars
-const VERSION = '1_0_1'
+const VERSION = '1_0_2'
 
 // The files to make available for offline use
 // eslint-disable-next-line no-unused-vars
@@ -15,3 +15,18 @@ const URLS = [
   `${GHP_PATH}/index.html`,
   `${GHP_PATH}/src/index.js`
 ]
+
+// https://stackoverflow.com/questions/46541071/progressive-web-app-does-not-work-offline-error
+self.addEventListener('fetch', function (event) {
+  event.respondWith(async function () {
+    try {
+      var res = await fetch(event.request)
+      var cache = await caches.open('cache')
+      cache.put(event.request.url, res.clone())
+      return res
+    }
+    catch (error) {
+      return caches.match(event.request)
+    }
+  }())
+})
