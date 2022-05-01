80bfd243e3c8df567118b2219c9cbce31b37b06a
RollingHog <46373241+RollingHog@users.noreply.github.com>
2022-05-01 05:15:47 +0300

    some comment removal

--- src/sw.js
+++ src/sw.js
@@ -1,17 +1,15 @@
-// Change this to your repository name
 const GHP_PATH = '/dozen-gs-stat-counter'
 
-// Choose a different app prefix name
+// eslint-disable-next-line no-unused-vars
 const APP_PREFIX = 'rh_ghp_dsc'
 
 // The version of the cache. Every time you change any of the files
 // you need to change this version (version_01, version_02…).
-// If you don't change the version, the service worker will give your
-// users the old files!
+// eslint-disable-next-line no-unused-vars
 const VERSION = '1_0_0'
 
-// The files to make available for offline use. make sure to add
-// others to this list
+// The files to make available for offline use
+// eslint-disable-next-line no-unused-vars
 const URLS = [
   `${GHP_PATH}/`,
   `${GHP_PATH}/index.html`,
